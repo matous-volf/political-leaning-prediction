458cf61645ae947e86417aec48b7ebd9287e7ee1
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-10-26 23:33:11 +0200

    style: formatting

--- utils/datasets.py
+++ utils/datasets.py
@@ -14,10 +14,16 @@ DATASETS_DIRECTORY = "../datasets/preprocessed"
 
 
 def get_datasets() -> List[Dataset]:
-    return sorted(map(
-        lambda filename: Dataset(
-            os.path.splitext(filename)[0],
-            pd.read_parquet(open(os.path.join(DATASETS_DIRECTORY, filename), "rb"))
+    return sorted(
+        map(
+            lambda filename: Dataset(
+                os.path.splitext(filename)[0],
+                pd.read_parquet(open(os.path.join(DATASETS_DIRECTORY, filename), "rb")),
+            ),
+            filter(
+                lambda filename: filename.endswith(".parquet"),
+                os.listdir(DATASETS_DIRECTORY),
+            ),
         ),
-        filter(lambda filename: filename.endswith(".parquet"), os.listdir(DATASETS_DIRECTORY))
-    ), key=lambda dataset: dataset.name)
+        key=lambda dataset: dataset.name,
+    )
--- utils/models.py
+++ utils/models.py
@@ -3,8 +3,14 @@ from enum import Enum
 from typing import List
 
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, DistilBertForSequenceClassification, \
-    RobertaTokenizer, BertTokenizer, pipeline
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DistilBertForSequenceClassification,
+    RobertaTokenizer,
+    BertTokenizer,
+    pipeline,
+)
 
 
 class Leaning(Enum):
@@ -14,10 +20,20 @@ class Leaning(Enum):
 
 
 class Model(ABC):
-    def __init__(self, tokenizer, model, supports_center_leaning: bool, model_max_length: int | None = None):
+    def __init__(
+        self,
+        tokenizer,
+        model,
+        supports_center_leaning: bool,
+        model_max_length: int | None = None,
+    ):
         self.tokenizer = tokenizer
         self.model = model
-        self.model_max_length = self.tokenizer.model_max_length if model_max_length is None else model_max_length
+        self.model_max_length = (
+            self.tokenizer.model_max_length
+            if model_max_length is None
+            else model_max_length
+        )
         self.name = type(self).__name__
         self.supports_center_leaning = supports_center_leaning
 
@@ -25,10 +41,15 @@ class Model(ABC):
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
         pass
 
-    def get_tokens(self, article_body: str, truncate_tokens: bool, return_tensors: str | None = "pt"):
+    def get_tokens(
+        self,
+        article_body: str,
+        truncate_tokens: bool,
+        return_tensors: str | None = "pt",
+    ):
         tokenizer_args = {
             "max_length": self.model_max_length,
-            "truncation": truncate_tokens
+            "truncation": truncate_tokens,
         }
 
         if return_tensors:
@@ -45,60 +66,81 @@ class PoliticalBiasBert(Model):
     def __init__(self):
         super().__init__(
             AutoTokenizer.from_pretrained("bert-base-cased"),
-            AutoModelForSequenceClassification.from_pretrained("bucketresearch/politicalBiasBERT"),
-            True
+            AutoModelForSequenceClassification.from_pretrained(
+                "bucketresearch/politicalBiasBERT"
+            ),
+            True,
         )
 
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
         tokens = self.get_tokens(article_body, truncate_tokens)
         output = self.get_output(tokens)
-        return [Leaning.LEFT, Leaning.CENTER, Leaning.RIGHT][torch.argmax(output.logits, dim=-1)]
+        return [Leaning.LEFT, Leaning.CENTER, Leaning.RIGHT][
+            torch.argmax(output.logits, dim=-1)
+        ]
 
 
 class PoliticalBiasPredictionAllsidesDeberta(Model):
     def __init__(self):
         super().__init__(
-            AutoTokenizer.from_pretrained("premsa/political-bias-prediction-allsides-DeBERTa"),
-            AutoModelForSequenceClassification.from_pretrained("premsa/political-bias-prediction-allsides-DeBERTa"),
+            AutoTokenizer.from_pretrained(
+                "premsa/political-bias-prediction-allsides-DeBERTa"
+            ),
+            AutoModelForSequenceClassification.from_pretrained(
+                "premsa/political-bias-prediction-allsides-DeBERTa"
+            ),
             True,
-            512
+            512,
         )
 
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
-        pipe = pipeline("text-classification", model=self.model, tokenizer=self.tokenizer)
+        pipe = pipeline(
+            "text-classification", model=self.model, tokenizer=self.tokenizer
+        )
         output = pipe(article_body)
         return {
             "LABEL_0": Leaning.LEFT,
             "LABEL_1": Leaning.CENTER,
-            "LABEL_2": Leaning.RIGHT
+            "LABEL_2": Leaning.RIGHT,
         }[output[0]["label"]]
+
 
 class DistilBertPoliticalBias(Model):
     def __init__(self):
         super().__init__(
             RobertaTokenizer.from_pretrained("cajcodes/DistilBERT-PoliticalBias"),
-            DistilBertForSequenceClassification.from_pretrained("cajcodes/DistilBERT-PoliticalBias"),
+            DistilBertForSequenceClassification.from_pretrained(
+                "cajcodes/DistilBERT-PoliticalBias"
+            ),
             True,
-            512
+            512,
         )
 
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
-        article_body = article_body.replace("\n", "").replace("``", "\"")
+        article_body = article_body.replace("\n", "").replace("``", '"')
         tokens = self.get_tokens(article_body, truncate_tokens)
         unk_token_id = self.tokenizer.convert_tokens_to_ids(self.tokenizer.unk_token)
-        tokens["input_ids"][tokens["input_ids"] >= self.model.config.vocab_size] = unk_token_id
+        tokens["input_ids"][
+            tokens["input_ids"] >= self.model.config.vocab_size
+        ] = unk_token_id
         output = self.get_output(tokens)
-        return [Leaning.RIGHT, Leaning.RIGHT, Leaning.CENTER, Leaning.LEFT, Leaning.LEFT][
-            torch.argmax(output.logits, dim=-1)
-        ]
+        return [
+            Leaning.RIGHT,
+            Leaning.RIGHT,
+            Leaning.CENTER,
+            Leaning.LEFT,
+            Leaning.LEFT,
+        ][torch.argmax(output.logits, dim=-1)]
 
 
 class BertPoliticalBiasFinetune(Model):
     def __init__(self):
         super().__init__(
             BertTokenizer.from_pretrained("bert-base-uncased"),
-            AutoModelForSequenceClassification.from_pretrained("jhonalevc1995/BERT-political_bias-finetune"),
-            False
+            AutoModelForSequenceClassification.from_pretrained(
+                "jhonalevc1995/BERT-political_bias-finetune"
+            ),
+            False,
         )
 
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
@@ -111,15 +153,19 @@ class DistilBertPoliticalFinetune(Model):
     def __init__(self):
         super().__init__(
             AutoTokenizer.from_pretrained("harshal-11/DistillBERT-Political-Finetune"),
-            AutoModelForSequenceClassification.from_pretrained("harshal-11/DistillBERT-Political-Finetune"),
+            AutoModelForSequenceClassification.from_pretrained(
+                "harshal-11/DistillBERT-Political-Finetune"
+            ),
             True,
-            512
+            512,
         )
 
     def predict(self, article_body: str, truncate_tokens: bool) -> Leaning:
         tokens = self.get_tokens(article_body, truncate_tokens)
         output = self.get_output(tokens)
-        return [Leaning.LEFT, Leaning.CENTER, Leaning.RIGHT][torch.argmax(output.logits, dim=-1)]
+        return [Leaning.LEFT, Leaning.CENTER, Leaning.RIGHT][
+            torch.argmax(output.logits, dim=-1)
+        ]
 
 
 def get_models() -> List[Model]:
@@ -128,5 +174,5 @@ def get_models() -> List[Model]:
         PoliticalBiasPredictionAllsidesDeberta(),
         DistilBertPoliticalBias(),
         BertPoliticalBiasFinetune(),
-        DistilBertPoliticalFinetune()
+        DistilBertPoliticalFinetune(),
     ]
